3e968a7b0dd2a737ff57f96ec7f2dc88cedc8acc
ajaymagar_45 <ajaymagar812@gmail.com>
2026-02-12 14:06:19 +0530

    @123

--- src/Practise/P1.java
+++ src/Practise/P1.java
@@ -5,21 +5,21 @@ import java.util.Iterator;
 
 public class P1 {
     public static void main(String [] args){
-        int $[]=new int [10];
-        $[0]=1;
-        $[1]=1;
-        $[2]=3;
-        $[3]=5;
-        $[4]=6;
-        $[5]=7;
-        $[6]=3;
-        $[7]=5;
-        $[8]=6;
-        $[9]=5;
-        for(int s:$)
+        int $_arr[]=new int [10];
+        $_arr[0]=1;
+        $_arr[1]=1;
+        $_arr[2]=3;
+        $_arr[3]=5;
+        $_arr[4]=6;
+        $_arr[5]=7;
+        $_arr[6]=3;
+        $_arr[7]=5;
+        $_arr[8]=6;
+        $_arr[9]=5;
+        for(int s:$_arr)
 
-        System.out.println("Array with duplicates : " + $[s]);
-        Iterator i= Arrays.stream($).distinct().iterator();
+        System.out.println("Array with duplicates : " + $_arr[s]);
+        Iterator i= Arrays.stream($_arr).distinct().iterator();
         while (i.hasNext())
         System.out.println("Array without duplicates : "+i.next());
     }
---
+++ src/Practise/StratsWithANumber.java
@@ -0,0 +1,12 @@
+package Practise;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StratsWithANumber {
+    public static void main(String[] args) {
+        List<String> listOfStrings = Arrays.asList("One", "2wo", "3hree", "Four", "5ive", "Six");
+        listOfStrings.stream().filter(i->Character.isDigit(i.charAt(0))).forEach(System.out::println);
+
+    }
+}

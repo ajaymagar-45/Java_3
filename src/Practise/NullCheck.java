1ee1157e05c886bdfe33e89b811b83b4bbb6c2b6
ajaymagar_45 <ajaymagar812@gmail.com>
2026-02-21 16:52:48 +0530

    _3

--- src/Practise/NullCheck.java
+++ src/Practise/NullCheck.java
@@ -4,16 +4,25 @@ import java.util.*;
 
 public class NullCheck {
     public static void main(String[] args) {
-        List al=new ArrayList<>();
-        al.add("Ravi");
-        al.add(10);
-        al.add(10);
-        al.add("Ravi");
-        System.out.println( "Size of the given list : "+ al.size());
-        System.out.println("o/p :"+al);
-        Set set=new HashSet(al);
-        System.out.println(" Removed duplicate : "+set);
-        System.out.println(set.size());
+        List al=new ArrayList();
+        al.add("A");
+        al.add("B");
+        al.add("C");
+        al.add("D");
+//        System.out.println( "Size of the given list : "+ al.size());
+//        System.out.println("o/p :"+al);
+//        Set set=new HashSet(al);
+//        System.out.println(" Removed duplicate : "+set);
+//        System.out.println(set.size());
+//
+//        //Convert this arraylist into map
+        Map<Integer,Object> map=new HashMap<>();
+        for (int i=0;i<al.size();i++){
+            map.put(i,al.get(i));
+
+        }
+        System.out.println(map);
+
 
 //        for(String a:al) {
 //            Optional<String> optional = Optional.ofNullable(a);

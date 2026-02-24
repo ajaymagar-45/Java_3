a9f87fa2cb0219795489e11b46e069c14332e054
ajaymagar_45 <ajaymagar812@gmail.com>
2026-02-24 17:33:35 +0530

    Commit of the day

---
+++ src/Practise/HashMapDemo.java
@@ -0,0 +1,29 @@
+package Practise;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class HashMapDemo {
+    public static void main(String[] args) {
+        Map<Integer,Object> map=new HashMap();
+        map.put(1,"Ajay");
+        map.put(2,"ajay");
+        map.put(4,'a');
+        map.put(7,'d');
+        map.put(9898,"xg");
+        map.put(9384,'d');
+        for(Map.Entry<Integer,Object>k_v:map.entrySet()){
+            System.out.println("KeySet : "+k_v.getKey()+" "+" ValueSet : "+k_v.getValue());
+
+
+        }
+        Set<Integer> set=map.keySet();
+        System.out.println(set);
+        Collection<Object> value=map.values();
+        System.out.println(value);
+        //To get keys and values from map
+
+    }
+}
